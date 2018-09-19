3becf0680507a403c3143fcca7c1a738435695ca
David Hamilton <daloach20@gmail.com>
2018-09-19 14:08:34 -0500

    Revert "Revert "messing around with pgp instead of pg""
    This reverts commit 30e5a9b9f95d69ac8bed9737ef9c2dbcb041491d.

--- app.js
+++ app.js
@@ -20,34 +20,8 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Connect to MySQL database with the correct user info and which database is to be used
-/*var connection = mysql.createConnection({
-  host      : 'localhost',
-  user      : 'root',
-  password  : 'jakejohn',
-  database  : 'warehouse_inventory'
-});
-
-//Connect to warehouse_inventory DB
-connection.connect();
-*/
-
-const { Client } = require('pg');
-
-const connection = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-connection.connect();
-
-/*connection.query('CREATE TABLE inventory', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  connection.end();
-});
-*/
+var pgp = require('pg-promise');
+var db = pgp(process.env.DATABASE_URL);
 
 //ROUTES
 //Home
@@ -68,7 +42,7 @@ app.post('/inventory', function(req, res) {
   console.log("inventory post route...now adding new item to DB");
   var q = insertQuery(item);
   console.log(q);
-  connection.query(q, function(err, results) {
+  db.query(q, function(err, results) {
     if(err) throw err;
   });  
   res.redirect("/inventory");
@@ -81,12 +55,13 @@ app.get('/inventory', function(req, res) {
   //console.log(passedStuff);
   //Query to get the data
   var q = 'SELECT * FROM inventory LIMIT 100';
-  connection.query(q, function(err, results) {
-    if(err) throw err;
-    
-    //Send the rendered page
-    res.render("inventory", {items: results});
-  });  
+  db.one(q)
+	.then(function(data){
+	console.log('DATA:', data.value)
+	})
+	.catch(function(error){
+	console.log('ERROR:', error)
+	})
 });
 
 //The server
