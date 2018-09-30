820d8a3a895d0e78fbf70f60d3c0f41a37637f38
David Hamilton <daloach20@gmail.com>
2018-09-30 17:04:20 -0500

    Fixing Syntax errors.

--- inventory.sql
+++ inventory.sql
@@ -1,15 +1,4 @@
 CREATE TABLE inventory(
-  inv_id INT NOT NULL SERIAL,
-  inv_items INT NOT NULL REFERENCES item(item_id),
-  description VARCHAR(100) NOT NULL,
-  category VARCHAR(25) DEFAULT 'unknown',
-  date_recieved TIMESTAMP DEFAULT NOW(),
-  storage_location VARCHAR(4) NOT NULL,
-  present VARCHAR(5) NOT NULL,
-  reserved VARCHAR(5) DEFAULT 'false',
-  PRIMARY KEY(inv_id)
-);
-CREATE TABLE inventory(
   inv_id SERIAL,
   description VARCHAR(100) NOT NULL,
   category VARCHAR(25) DEFAULT 'unknown',
@@ -20,14 +9,14 @@ CREATE TABLE inventory(
   PRIMARY KEY(inv_id)
 );
 CREATE TABLE item(
-  item_id INT NOT NULL SERIAL,
+  item_id SERIAL,
   description VARCHAR(100) NOT NULL,
   category VARCHAR(25) DEFAULT 'unknown',
   date_recieved TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY(item_id)
 );
 CREATE TABLE user(
-  user_id INT NOT NULL SERIAL,
+  user_id SERIAL,
   user_name VARCHAR(100) NOT NULL,
   user_mobile VARCHAR(100) NOT NULL,
   user_email VARCHAR(100) NOT NULL,
@@ -37,7 +26,7 @@ CREATE TABLE user(
   PRIMARY KEY(user_id)
 );
 CREATE TABLE project(
-  project_id INT NOT NULL SERIAL,
+  project_id SERIAL,
   description VARCHAR(100) NOT NULL,
   project_items INT NOT NULL REFERENCES item(item_id),
   manager INT NOT NULL REFERENCES user(user_id),
