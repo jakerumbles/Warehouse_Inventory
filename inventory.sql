2a1cdd27af3824922420d6ed0ed194b69a7e8500
David Hamilton <daloach20@gmail.com>
2018-10-24 13:25:59 -0500

    Added inventory_history table.

--- inventory.sql
+++ inventory.sql
@@ -1,35 +1,30 @@
 CREATE TABLE inventory(
-  inv_id SERIAL,
-  description VARCHAR(100) NOT NULL,
-  category VARCHAR(25) DEFAULT 'unknown',
-  date_recieved DATE DEFAULT NOW(),
-  storage_location VARCHAR(4) NOT NULL,
-  present VARCHAR(5) NOT NULL,
-  reserved VARCHAR(5) DEFAULT 'false',
-  PRIMARY KEY(inv_id)
+    inv_id SERIAL,
+    description VARCHAR(100) NOT NULL,
+    category VARCHAR(25) DEFAULT 'unknown',
+    date_recieved DATE DEFAULT NOW(),
+    storage_location VARCHAR(4) NOT NULL,
+    present VARCHAR(5) NOT NULL,
+    reserved VARCHAR(5) DEFAULT 'false',
+    PRIMARY KEY(inv_id)
 );
-CREATE TABLE item(
-  item_id SERIAL,
-  description VARCHAR(100) NOT NULL,
-  category VARCHAR(25) DEFAULT 'unknown',
-  date_recieved TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY(item_id)
+CREATE TABLE "users"(
+    id uuid NOT NULL,
+    firstName VARCHAR(64),
+    lastName VARCHAR(64),
+    email VARCHAR(128),
+    password VARCHAR(60),
+    access INTEGER,
+    date_created DATE NOT NULL DEFAULT CURRENT_DATE
+    PRIMARY KEY(item_id)
 );
-CREATE TABLE user_account(
-  user_id SERIAL,
-  user_name VARCHAR(100) NOT NULL,
-  user_mobile VARCHAR(100) NOT NULL,
-  user_email VARCHAR(100) NOT NULL,
-  user_address VARCHAR(100) NOT NULL,
-  date_started TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY(user_id)
+CREATE TABLE inventory_history(
+    hist_id SERIAL,
+    inv_id INTEGER NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    category VARCHAR(25) NOT NULL,
+    date_recieved DATE DEFAULT NOW(),
+    storage_location VARCHAR(4) NOT NULL,
+    history VARCHAR(100),
+    PRIMARY KEY(hist_id)
 );
-CREATE TABLE project(
-  project_id SERIAL,
-  description VARCHAR(100) NOT NULL,
-  project_items INT NOT NULL REFERENCES item(item_id),
-  manager INT NOT NULL REFERENCES user_account(user_id),
-  date_started TIMESTAMP DEFAULT NOW(),
-  PRIMARY KEY(project_id)
-);
-ALTER TABLE user_account ADD COLUMN manages_project INT REFERENCES project(project_id);
